12a7e41635af2ea232d45efb19170aa6397f5c92
Yousef Herwees <herwees@amazon.com>
2021-12-10 11:00:31 -0800

    Spawning an item in center of the room, image url doesnt work though

--- src/systems/inventory-system.js
+++ src/systems/inventory-system.js
@@ -8,7 +8,7 @@ export class InventorySystem {
   onSpawnInventory = () => {
     const entity = document.createElement("a-entity");
     this.scene.appendChild(entity);
-    entity.setAttribute("offset-relative-to", { target: "#avatar-pov-node", offset: { x: 0, y: 0, z: -1.5 } });
+    entity.setAttribute("offset-relative-to", { target: "#environment-root", offset: { x: 0, y: 0, z: -1.5 } });
     entity.setAttribute("networked", { template: "#inventory-object-media" });
   }
 }
